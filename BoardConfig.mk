4a02051fea113a749131c08c18bdcbe2db5816e4
Codenamedroid <bemegadroid@gmail.com>
2012-09-18 20:09:34 -0500

    disable inline kernel building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,5 +30,6 @@
 TARGET_OTA_ASSERT_DEVICE := d2att
 
 # Kernel
-TARGET_KERNEL_CONFIG		:= cyanogen_d2_defconfig
+#TARGET_KERNEL_CONFIG       := cyanogen_d2_defconfig
+TARGET_PREBUILT_KERNEL      := device/samsung/d2att/kernel
 
